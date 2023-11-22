873f259ed405d480e9fae8aedc9b89bb2e5be905
adamwisnup <wisnuadam21@gmail.com>
2023-11-22 15:04:02 +0700

    change respon json

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -7,21 +7,21 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json([
-        {
-            "id": 1,
-            "nama": "Adam Wisnu",
-            "kelas": "TT-45-09"
-        },
-        {
-            "id": 2,
-            "nama": "Raihan Putra",
-            "kelas": "TT-45-11"
-        }
-    ]);
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      nama: "Yanto",
+      kelas: "TT-40-01",
+    },
+    {
+      id: 2,
+      nama: "Sugito",
+      kelas: "TT-42-02",
+    },
+  ]);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
